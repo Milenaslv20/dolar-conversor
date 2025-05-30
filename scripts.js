2ed0fa30be7790be1c6ef00ddecea6bf7e2083de
Milenaslv20 <milenaslvmedeiros@hotmail.com>
2025-05-29 20:15:05 -0400

    feature: current date

--- scripts.js
+++ scripts.js
@@ -5,17 +5,24 @@ const text1 = document.getElementById('text_1');
 const text2 = document.getElementById('text_2');
 const data = document.getElementById('data');
 
+const dateNow = new Date();
+const monthNowBR = dateNow.toLocaleString('pt-BR', { month: 'long' });
+const monthNowUS = dateNow.toLocaleString('en-US', { month: 'long' });
+const dayNow = dateNow.getDate();
+
+const monthDayBR = `${dayNow} de ${monthNowBR.charAt(0).toUpperCase() + monthNowBR.slice(1)}`;
+const monthDayUS = `${monthNowUS.charAt(0).toUpperCase() + monthNowUS.slice(1)} ${dayNow}`;
 
 let messages = {
     english: {
         text1: "1 United States Dollar",
         text2: "5.66 Brazilian Real",
-        data: "May 6"
+        data: monthDayUS
     },
     portuguese: {
-        text1: "1 dolar estadunidense",
-        text2: "5.66 reais brasileiros",
-        data: "Maio 6"
+        text1: "1 Dólar Estadunidense",
+        text2: "5.66 Reais Brasileiros",
+        data: monthDayBR
     }
 };
 
@@ -25,12 +32,12 @@ window.addEventListener('DOMContentLoaded', () => {
     data.textContent = messages['portuguese'].data;
 });
 
-let currentLanguage = "english"
+let currentLanguage = "english";
 function languageChange(language){
     currentLanguage = language;
 
     text1.textContent = messages[currentLanguage].text1;
     text2.textContent = messages[currentLanguage].text2;
     data.textContent = messages[currentLanguage].data;
-}
+};
 
